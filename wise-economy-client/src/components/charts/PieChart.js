7ea7d01d63518cae3f7232032309d986182e1bea
aschalegn <aschaley1@gmail.com>
2020-02-02 22:07:26 +0200

    pie chart

--- wise-economy-client/src/components/charts/PieChart.js
+++ wise-economy-client/src/components/charts/PieChart.js
@@ -2,13 +2,60 @@ import React, { useRef, useEffect, useContext } from 'react'
 import * as d3 from "d3";
 import { BudgetContext } from '../../contexts/budgetContext';
 
-
-export default function PieChart() {
+const Chart = props => {
     const width = 400;
     const height = 400;
-    const r = 100
+    const r = 100;
     const chartSection = useRef(null);
+    const data = [
+        { "label": "income", sum: props.INCOM_SUM },
+        { "label": "outcome", sum: props.OUTCOME_SUM },
+    ]
+
+    if (chartSection.current) {
+        const color = d3.scaleOrdinal()
+            .range(["red", "blue", "orange"])
+        const svg = d3.select(chartSection.current)
+            .attr("width", width)
+            .attr("height", height);
+
+        const grouop = svg.append("g")
+            .attr("transform", "translate(200, 200)");
+
+        const arc = d3.arc()
+            .innerRadius(50)
+            .outerRadius(r);
+
+        const pie = d3.pie()
+            .value(d => { return d.sum });
+
+        const arcs = grouop.selectAll(".arc")
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc");
+
+        arcs.append("path")
+            .attr("d", arc)
+            .attr("fill", (d) => { return color(d.data) })
+
+        console.log(pie(data));
+
+    }
+
+    return (
+        < svg ref={chartSection} >
+
+        </svg >
+    )
+}
+
+export default function PieChart() {
+
+
+
     const { budgetState } = useContext(BudgetContext);
+
     const calculateSum = (array) => {
         let sum = 0;
         array.forEach(element => {
@@ -19,25 +66,17 @@ export default function PieChart() {
 
     const INCOM_SUM = calculateSum(budgetState.income);
     const OUTCOME_SUM = calculateSum(budgetState.outcome);
+    // useEffect(()=>{
+    //     INCOM_SUM = calculateSum(budgetState.income);
+    //    OUTCOME_SUM = calculateSum(budgetState.outcome);
+    // },[]);
 
-    const data = [
-        { "label": "income", sum: INCOM_SUM },
-        { "label": "outcome", sum: OUTCOME_SUM },
-    ]
 
-    useEffect(() => {
-        if (chartSection.current) {
-            const svg = d3.select(chartSection.current)
-                .append("svg")
-                .attr("viewBox", [-width / 2, -height / 2, width, height]);
-        }
-    }, [])
+
 
     return (
         <section >
-            <svg ref={chartSection}>
-
-            </svg>
+            <Chart INCOM_SUM={INCOM_SUM} OUTCOME_SUM={OUTCOME_SUM} />
         </section>
     )
 }
